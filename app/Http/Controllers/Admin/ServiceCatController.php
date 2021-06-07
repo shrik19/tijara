69b1126fd389597821b6ef274332123c95993be9
kevin <kevin@telemar.it>
2021-06-07 15:18:03 +0530

    admin service cat issue

--- app/Http/Controllers/Admin/ServiceCatController.php
+++ app/Http/Controllers/Admin/ServiceCatController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-use App\Models\servicecategories;
+use App\Models\ServiceCategories;
 use App\Models\ServiceSubcategories;
 /*Uses*/
 use Session;

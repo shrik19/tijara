9a34825422d935703e2ed97e4387ee5ee40de1ae
kevin <kevin@telemar.it>
2021-07-07 11:59:11 +0530

    klarna payment blades

---
+++ resources/views/Front/Packages/payment_error.blade.php
@@ -0,0 +1,31 @@
+@extends('Front.layout.template')
+@section('middlecontent')
+
+<div class="containerfluid">
+  <div class="col-md-6 hor_strip debg_color">
+  </div>
+  <div class="col-md-6 hor_strip gray_bg_color">
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="">
+        </br>     
+      
+      <div class="col-md-12">
+      	<div class="card">
+			<div class="card-header row">
+				
+			</div>
+			<div class="card-body">
+				<h1 style="text-align: center;color: red">{{$error_messages}}</h1>
+				<a href="{{url('/')}}/seller-packages" class="btn btn-black debg_color login_btn" style="margin-left: 450px;">{{ __('lang.try_again_btn')}}</a>
+			</div>
+		</div>
+      </div>
+
+    </div>
+  </div>
+</div> <!-- /container -->
+
+@endsection

a4e0c9f1e35b090642f5b1945caf8f3929e5d92d
Dhiraj Sonar <cooldhirajsonar@gmail.com>
2021-07-09 16:59:33 +0530

    Css changes for product Attributes

--- resources/views/Front/product_details.blade.php
+++ resources/views/Front/product_details.blade.php
@@ -55,6 +55,124 @@
 }
 
 .small-container .show-small-img:last-of-type { margin-right: 0; }
+
+.btn span.glyphicon {    			
+	opacity: 0;				
+}
+.btn.active span.glyphicon {				
+	opacity: 1;				
+}
+
+
+.funkyradio div {
+  clear: both;
+  overflow: hidden;
+}
+
+.funkyradio label {
+  width: 100%;
+  border-radius: 3px;
+  border: 1px solid #D1D3D4;
+  font-weight: normal;
+}
+
+.funkyradio input[type="radio"]:empty,
+.funkyradio input[type="checkbox"]:empty {
+  display: none;
+}
+
+.funkyradio input[type="radio"]:empty ~ label,
+.funkyradio input[type="checkbox"]:empty ~ label {
+  position: relative;
+  line-height: 2.5em;
+  text-indent: 3.25em;
+  /*margin-top: 2em;*/
+  cursor: pointer;
+  -webkit-user-select: none;
+     -moz-user-select: none;
+      -ms-user-select: none;
+          user-select: none;
+}
+
+.funkyradio input[type="radio"]:empty ~ label:before,
+.funkyradio input[type="checkbox"]:empty ~ label:before {
+  position: absolute;
+  display: block;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  content: '';
+  width: 2.5em;
+  background: #D1D3D4;
+  border-radius: 3px 0 0 3px;
+}
+
+.funkyradio input[type="radio"]:hover:not(:checked) ~ label,
+.funkyradio input[type="checkbox"]:hover:not(:checked) ~ label {
+  color: #888;
+}
+
+.funkyradio input[type="radio"]:hover:not(:checked) ~ label:before,
+.funkyradio input[type="checkbox"]:hover:not(:checked) ~ label:before {
+  content: '\2714';
+  text-indent: .9em;
+  color: #C2C2C2;
+}
+
+.funkyradio input[type="radio"]:checked ~ label,
+.funkyradio input[type="checkbox"]:checked ~ label {
+  color: #777;
+}
+
+.funkyradio input[type="radio"]:checked ~ label:before,
+.funkyradio input[type="checkbox"]:checked ~ label:before {
+  content: '\2714';
+  text-indent: .9em;
+  color: #333;
+  background-color: #ccc;
+}
+
+.funkyradio input[type="radio"]:focus ~ label:before,
+.funkyradio input[type="checkbox"]:focus ~ label:before {
+  box-shadow: 0 0 0 3px #999;
+}
+
+.funkyradio-default input[type="radio"]:checked ~ label:before,
+.funkyradio-default input[type="checkbox"]:checked ~ label:before {
+  color: #333;
+  background-color: #ccc;
+}
+
+.funkyradio-primary input[type="radio"]:checked ~ label:before,
+.funkyradio-primary input[type="checkbox"]:checked ~ label:before {
+  color: #fff;
+  background-color: #337ab7;
+}
+
+.funkyradio-success input[type="radio"]:checked ~ label:before,
+.funkyradio-success input[type="checkbox"]:checked ~ label:before {
+  color: #fff;
+  background-color: #5cb85c;
+}
+
+.funkyradio-danger input[type="radio"]:checked ~ label:before,
+.funkyradio-danger input[type="checkbox"]:checked ~ label:before {
+  color: #fff;
+  background-color: #d9534f;
+}
+
+.funkyradio-warning input[type="radio"]:checked ~ label:before,
+.funkyradio-warning input[type="checkbox"]:checked ~ label:before {
+  color: #fff;
+  background-color: #f0ad4e;
+}
+
+.funkyradio-info input[type="radio"]:checked ~ label:before,
+.funkyradio-info input[type="checkbox"]:checked ~ label:before {
+  color: #fff;
+  background-color: #5bc0de;
+}
+
 </style>
 
 <script src="{{url('/')}}/assets/front/js/zoom-image.js"></script>
@@ -122,17 +240,19 @@
                                     <input type="hidden" id="variant_type" name="variant_type" value="{{$attribute['attribute_type']}}">
                                     @if($attribute['attribute_type']=='radio')
                                     <ul class="select_product_color {{$attribute_id}}">
+                                       
+                                        <div class="funkyradio">
                                         @foreach($attribute['attribute_values'] as $attribute_value_id=>$attribute_value)
-                                        <div class="squaredThree">
-                                            <input style="background-color:{{$attribute_value}};" type="checkbox" class="product_checkbox_attribute" name="optionsRadios" product_id="{{$Product->id}}" id="{{$attribute_value_id}}" value="other" data-variant="{{$attribute['variant_values'][$attribute_value_id]}}"  >
-                                            <span class="custom-control-indicator"></span>
-                                            <label style="background-color:{{$attribute_value}};" for="{{$attribute_value_id}}">{{$attribute_value}} </label>
-                                        </div>
-
-                                        @endforeach
+                                          <div class="squaredThree funkyradio-success">
+                                              <input style="background-color:{{$attribute_value}};" class="product_checkbox_attribute" type="radio" name="optionsRadios" product_id="{{$Product->id}}" id="{{$attribute_value_id}}" value="other" data-variant="{{$attribute['variant_values'][$attribute_value_id]}}" />
+                                              <span class="custom-control-indicator"></span>
+                                              <label for="{{$attribute_value_id}}" style="background-color:{{$attribute_value}};"> {{$attribute_value}} </label>
+                                          </div>
+                                          @endforeach
+                                        </div>  
                                     </ul>
                                     @elseif($attribute['attribute_type']=='dropdown')
-                                    <select id="select_product_variant" class="{{$attribute_id}}">
+                                    <select id="select_product_variant" class="{{$attribute_id}} form-control">
                                         <option value="">{{ __('lang.select_label')}}</option>
                                     @foreach($attribute['attribute_values'] as $attribute_value_id=>$attribute_value)
                                         <option value="{{$attribute_value_id}}" data-variant="{{$attribute['variant_values'][$attribute_value_id]}}"> {{$attribute_value}} </option>
@@ -141,7 +261,7 @@
                                     @elseif($attribute['attribute_type']=='textbox')
                                     <div class="inputattributes {{$attribute_id}}">
                                     @foreach($attribute['attribute_values'] as $attribute_value_id=>$attribute_value)
-                                        <input type="text" value="{{$attribute_value_id}}" data-variant="{{$attribute['variant_values'][$attribute_value_id]}}"> {{$attribute_value}}
+                                        <input class="form-control" type="text" value="{{$attribute_value_id}}" data-variant="{{$attribute['variant_values'][$attribute_value_id]}}"> {{$attribute_value}}
                                         @endforeach
                                     </div>
                                     @endif

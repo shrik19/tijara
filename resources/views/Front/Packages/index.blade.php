37ef17d36911273a9065cb8f9d2448ee132c646b
kevin <kevin@telemar.it>
2021-07-09 16:24:28 +0530

    active button

--- resources/views/Front/Packages/index.blade.php
+++ resources/views/Front/Packages/index.blade.php
@@ -61,7 +61,7 @@
 					  			<td><a href="javascript:void(0)" class="btn btn-warning "> {{ __('users.not_activated_label')}}</a></td>
 					  		@elseif(($row->start_date >= date('Y-m-d H:i:s') && $row->payment_status=="checkout_incomplete"))
 					  		<td><a href="javascript:void(0)" class="btn btn-danger"> {{ __('lang.pending_label')}}</a></td>
-					  		@elseif(($row->start_date >= date('Y-m-d H:i:s') && $row->payment_status=="CAPTURED"))
+					  		@else
 					  			<td><a href="javascript:void(0)" class="btn btn-success "> {{ __('lang.active_label')}} </a></td>
 					  		@endif
 					    </tr>

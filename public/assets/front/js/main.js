26e21c15708f02403e8f1ba5425c000881dfab31
kevin <kevin@telemar.it>
2021-07-19 16:08:05 +0530

    added regex for product-slug

--- public/assets/front/js/main.js
+++ public/assets/front/js/main.js
@@ -8,9 +8,8 @@ function convertToSlug(inputtxt){
     //replace space with hypen
     slug = inputtxt.value.toLowerCase();
     //replace special character with -
-    slug = slug.replace(/[`~!@#$%^&*()_|+\-=?;:'",.-<>\{\}\[\]\\\/]/gi, '');
-
-    //slug = slug.replace(/[^\w\s]/gi, '-')
+    slug = slug.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+   // slug = slug.replace(/[^\w\s]/gi, '-')
     //function to check slug name is unique or not
     slug = checkUniqueSlugName(slug);
     $('.slug-name').val(slug);

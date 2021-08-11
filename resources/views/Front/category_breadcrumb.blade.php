7a832b2be966f4f05bfc23c5a24998f9522ed1fa
kevin <kevin@telemar.it>
2021-08-11 17:29:36 +0530

    added view

---
+++ resources/views/Front/category_breadcrumb.blade.php
@@ -0,0 +1,8 @@
+<ul class="breadcrumb-category">
+
+	@if(!empty($category_name))
+	  <li><a href="#">{{ __('lang.categories_head')}}</a></li>
+	  <li><a href="#" id="breadcrumb_category"> > {{ $category_name}}</a></li>
+	 @endif
+<!--   <li><a href="#" id="breadcrumb_subcategory">Request::segment(3) !='services'</a></li> -->
+</ul>

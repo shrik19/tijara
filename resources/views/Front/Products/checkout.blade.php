2041e3355be077f407d8cba9e3f9ad067b20a55d
Kevin <kevin@telemar.it>
2021-09-10 19:17:18 +0530

    swish

--- resources/views/Front/Products/checkout.blade.php
+++ resources/views/Front/Products/checkout.blade.php
@@ -10,7 +10,7 @@
   }
 </style>
 
-<section class="product_details_section">
+<section class="">
 <div class="loader"></div>
 <div class="container">
       <div class="row" style="margin-bottom:60px;">

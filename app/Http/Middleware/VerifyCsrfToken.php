4fc2f332b6b30d080087da529adf02efe2fa4d2d
Kevin <kevin@telemar.it>
2021-09-10 17:51:50 +0530

    swish

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -16,6 +16,7 @@ class VerifyCsrfToken extends Middleware
         'api/getPaymentMethods',
         'api/initiatePayment',
         'api/handleShopperRedirect',
-        'api/submitAdditionalDetails'
+        'api/submitAdditionalDetails',
+        'swish-ipn-callback'
     ];
 }

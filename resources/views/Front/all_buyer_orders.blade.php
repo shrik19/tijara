d2da3db4a859f105ff75d20d27baa057865b32b4
kevin <kevin@telemar.it>
2021-09-10 14:01:57 +0530

    added blades

---
+++ resources/views/Front/all_buyer_orders.blade.php
@@ -0,0 +1,152 @@
+@extends('Front.layout.template')
+@section('middlecontent')
+
+<div class="mid-section">
+<div class="container-fluid">
+  <div class="container-inner-section-1">
+  <!-- Example row of columns -->
+  
+  <div class="row">
+    @if($is_seller==1)
+      <div class="col-md-2 tijara-sidebar">
+        @include ('Front.layout.sidebar_menu')
+      </div>
+      <div class="col-md-10 tijara-content">
+        @else
+        <div class="col-md-12 tijara-content">
+      @endif
+      
+		 
+	  @include('Front.alert_messages')
+    <div class="seller_info">
+	  <div class="card">
+		<div class="card-header row seller_header">
+      <h2 class="page_heading">{{ __('users.my_order_title')}}</h2>
+      </div>
+    </div>
+    <div class="seller_mid_cont">
+      
+    <div class="col-md-12" style="margin-top: 20px;">
+      <div class="tijara-info-section">
+        <h1 class="buyer-prod-head">{{__('messages.info_head')}}</h1>
+        <p  class="buyer-prod-content">{{__('messages.my_order_info')}}</p>
+      </div>
+    </div>
+
+		<div class="col-md-12">
+		    
+		  
+		<div class="card-body">
+
+       <div class="card">
+                              <div class="col-md-12" style="margin-bottom: 40px;margin-top: -45px;">
+                                <div class="col-md-9"></div>
+                                    <div  class="col-md-3">
+                                        <form id="filter-buyer-order" action="{{route('frontAllBuyerOrders')}}" method="post">
+                                        @csrf
+                                        <?php echo $monthYearHtml;?>
+                                        </form>
+                                    </div>
+                              </div>
+                            <div class="card-body"  style="margin-top: 20px;">
+                                <div class="row">
+                                    @if(!empty($ordersDetails) && count($ordersDetails) > 0)
+
+                                    @foreach($ordersDetails as $key => $value)
+
+                                    <?php 
+
+                                  if(!empty($value->image)) {
+                                    $imagesParts    =   explode(',',$value->image); 
+                                    $image  =   url('/').'/uploads/ProductImages/resized/'.$value->image;
+                                    }
+                                    else{
+                                    $image  =     url('/').'/uploads/ProductImages/resized/no-image.png';
+                                    }
+                                   
+                                   
+                                    $dated      =   date('Y-m-d',strtotime($value->created_at));
+
+                                    $productName = (!empty($value->title)) ? $value->title : '-';
+                                    // $price = $value['service_price'];
+                                    $product_price = (!empty($value->price)) ? $value->price : '-';
+                                    $storeName = (!empty($value->store_name)) ?$value->store_name : '-';
+                                  
+                                  $seller_name = $value->fname." ".$value->lname;
+                
+                                  $seller_name = str_replace( array( '\'', '"', 
+                                  ',' , ';', '<', '>', '(', ')','$','.','!','@','#','%','^','&','*','+','\\' ), '', $seller_name);
+                                  $seller_name = str_replace(" ", '-', $seller_name);
+                                  $seller_name = strtolower($seller_name);
+                                              
+                         
+                                $seller_link= url('/').'/seller/'.$seller_name."/". base64_encode($value->seller_id)."/products"; 
+
+                               $product_link = url('/').'/product/'.$value->product_slug.'-P-'.$value->product_code;
+
+                                $id =  $value['id'];
+
+                                    /* $discount_price =0;
+                                    if(!empty($value['discount'])) {
+                                    $discount = number_format((($price * $value['discount']) / 100),2,'.','');
+                                    $discount_price = $price - $discount;
+                                    }
+                                    */
+                                    ?> 
+                                    <div class="col-sm-3">
+                                        <div class="card product-card">
+                                      
+                                            <img class="card-img-top buyer-product-img" src="{{$image}}" product_link="{{$product_link}}">
+                                            <div class="card-body">
+                                                <h5 class="card-title">{{$dated}}</h5>
+                                                <p class="card-text buyer-product-title"><a href="{{$product_link}}"  style="color: #000 !important">{{$productName}}</a></p>
+                                                <p class="card-text order-product-price">  
+                                                <span class="buyer-price" id="product_variant_price">
+                                                {{number_format($product_price,2) }} kr
+                                                </span> 
+                                                </p>
+                                                <p class="card-text order-product-title">{{ __('users.butik_btn')}} : <a href="{{$seller_link}}" style="color: #000 !important">{{$storeName}}</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    @endforeach
+
+                                    @else
+                                    <div style="text-align: center;margin-top: 50px;">{{__('lang.datatables.sEmptyTable')}}</div>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+
+  {!! $ordersDetails->links() !!}
+		</div>
+    </div>
+	  </div>
+		</div>
+    </div>
+ 
+  
+</div>
+  </div>
+</div>
+</div> <!-- /container -->
+<script src="{{url('/')}}/assets/front/js/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+<script src="{{url('/')}}/assets/front/js/dataTables.bootstrap4.min.js"></script>
+
+<!-- General JS Scripts -->
+<script src="{{url('/')}}/assets/js/sweetalert.js"></script>
+<script type="text/javascript">
+$(".buyer-product-img").click(function(){
+  var attr_val = $(this).attr('product_link');
+  if(attr_val !=''){
+    window.location.href = attr_val; 
+  }
+});
+
+$("#monthYear").on('change', function() {
+  //alert( this.value );
+  this.form.submit();
+});
+</script>
+@endsection
---
+++ resources/views/Front/buyer_booking_request.blade.php
@@ -0,0 +1,139 @@
+@extends('Front.layout.template')
+@section('middlecontent')
+
+<div class="mid-section">
+    <div class="container-fluid">
+        <div class="container-inner-section-1">
+        <!-- Example row of columns -->
+
+        <div class="row">
+            <div class="col-md-12 tijara-content">
+    
+                @include('Front.alert_messages')
+                <div class="seller_info">
+
+                    <div class="card">
+                        <div class="card-header row seller_header">
+                        <h2 class="page_heading">{{ __('users.my_booking_title')}}</h2>
+               
+                        </div>
+                    </div>
+                    <div class="seller_mid_cont"  style="margin-top: 20px;">
+                    
+                        <div class="col-md-12">
+                            <div class="tijara-info-section">
+                                <h1 class="buyer-prod-head">{{__('messages.info_head')}}</h1>
+                                <p  class="buyer-prod-content">{{__('messages.service_booking_msg')}}</p>
+                            </div>
+                        </div>
+                  
+                        <div class="card">
+                              <div class="col-md-12" style="margin-bottom: 40px;margin-top: -45px;">
+                                <div class="col-md-9"></div>
+                                    <div  class="col-md-3">
+                                        <form id="filter-service-booking" action="{{route('frontAllServiceRequest')}}" method="post">
+                                        @csrf
+                                        <?php echo $monthYearHtml;?>
+                                        </form>
+                                    </div>
+                              </div>
+                            <div class="card-body"  style="margin-top: 20px;">
+                                <div class="row">
+                                    @if(!empty($buyerBookingRequest) && count($buyerBookingRequest) > 0)
+
+                                    @foreach($buyerBookingRequest as $key => $value)
+
+                                    <?php 
+
+
+                                    $getStoreName = DB::table('users')
+                                    ->where('users.is_deleted','!=',1)
+                                    ->where('id','=',$value['seller_id'])
+                                    ->select('users.store_name','users.fname','users.lname')
+                                    ->get();
+
+
+                                      if(!empty($value['image'])) {
+                                    $imagesParts    =   explode(',',$value['images']); 
+                                    $image  =   url('/').'/uploads/ServiceImages/resized/'.$value['images'];
+                                    }
+                                    else{
+                                    $image  =     url('/').'/uploads/ServiceImages/resized/no-image.png';
+                                    }
+                                   
+                                    $dated      =   date('Y-m-d',strtotime($value['service_date']));
+
+                                    $serviceName = (!empty($value['title'])) ? $value['title'] : '-';
+                              
+                                    $service_price = (!empty($value->price)) ? $value->price : '-';
+                                    $storeName = (!empty($getStoreName[0]->store_name)) ?$getStoreName[0]->store_name : '-';
+
+                                    $id =  $value['id'];
+
+                                $seller_name = $getStoreName[0]->fname." ".$getStoreName[0]->lname;
+                 
+                                $seller_name = str_replace( array( '\'', '"', 
+                                  ',' , ';', '<', '>', '(', ')','$','.','!','@','#','%','^','&','*','+','\\' ), '', $seller_name);
+                                $seller_name = str_replace(" ", '-', $seller_name);
+                                $seller_name = strtolower($seller_name);
+
+                                $seller_link= url('/').'/seller/'.$seller_name."/". base64_encode($value->seller_id)."/services"; 
+
+                                $service_link = url('/').'/service/'.$value->service_slug.'-S-'.$value->service_code;
+                                   
+                                    ?> 
+                                    <div class="col-sm-3">
+                                        <div class="card product-card">
+                                            <img class="card-img-top buyer-product-img ServiceImgCard" src="{{$image}}" service_link="{{$service_link}}">
+                                            <div class="card-body">
+                                                <h5 class="card-title">{{$dated}}</h5>
+                                                <p class="card-text buyer-product-title"><a href="{{$service_link}}"  style="color: #000 !important">{{$serviceName}}</a></p>
+                                                <p class="card-text" style="margin-bottom: 50px;margin-top: -36px;">  
+                                                <span class="buyer-price  booking-service-price" id="product_variant_price">
+                                                {{number_format($service_price,2) }} kr
+                                                </span> 
+                                                </p>
+                                                <p class="card-text buyer-service-store">{{ __('users.butik_btn')}} :<a href="{{$seller_link}}" style="color: #000 !important">{{$storeName}}</a></p>
+
+
+                                                <div class="buyer-button">
+                                                <a href="javascript:void(0)" onclick='return ConfirmDeleteFunction("{{route("frontServiceRequestDel", base64_encode($id))}}");'  title="{{ __('lang.remove_title')}}" class="btn btn-black btn-sm login_btn remove-btn-col">{{ __('users.cancel_service')}}</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    @endforeach
+
+                                    @else
+                                    <div style="text-align: center;margin-top: 50px;">{{__('lang.datatables.sEmptyTable')}}</div>
+                                    @endif
+                                </div>
+                            </div>
+                              {!! $buyerBookingRequest->links() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+        </div>       
+    </div>
+</div> <!-- /container -->
+<script src="{{url('/')}}/assets/front/js/jquery-3.3.1.min.js" crossorigin="anonymous"></script>
+<!-- Template CSS -->
+<link rel="stylesheet" href="{{url('/')}}/assets/css/sweetalert.css">
+<!-- General JS Scripts -->
+<script src="{{url('/')}}/assets/js/sweetalert.js"></script>
+<script type="text/javascript">
+$(".ServiceImgCard").click(function(){
+  var attr_val = $(this).attr('service_link');
+  if(attr_val !=''){
+    window.location.href = attr_val; 
+  }
+});
+$("#monthYear").on('change', function() {
+  //alert( this.value );
+  this.form.submit();
+});
+</script>
+@endsection

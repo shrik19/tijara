965ca0b3ac80b13c6a5149f31ef0cbb8d03caabd
landeshrikant <landeshrikant@gmail.com>
2021-09-21 23:15:15 +0530

    update as per desing

--- resources/views/Front/seller-products.blade.php
+++ resources/views/Front/seller-products.blade.php
@@ -20,7 +20,7 @@
              @if(!empty($logo)) 
              <div class="seller_logo">
              <img class="seller_logo" src="{{$logo}}" alt="Logo" />&nbsp;&nbsp;</div>@endif
-             <div class="seller_info">
+             <div class="seller_info border-none">
               <h2>{{ $seller_name }}</h2>
               <p>{{ $city_name }}</p>
               <div class="star-rating">

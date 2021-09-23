d722b6ad8e300c488bdd79e3e1d77d62de797354
landeshrikant <landeshrikant@gmail.com>
2021-09-23 16:36:26 +0530

    resolved confilts

--- resources/views/Front/buyer_product_details.blade.php
+++ resources/views/Front/buyer_product_details.blade.php
@@ -56,7 +56,7 @@
 
                     <div class="quantity_box">              
                                <span  style="font-size:20px;color: #03989e; font-weight:600;" id="product_variant_price"><span style="@if(!empty($first['discount_price'])) text-decoration: line-through; @endif">{{ number_format($first['price'],2) }} kr</span> @if(!empty($first['discount_price'])) &nbsp;&nbsp;{{ number_format($first['discount_price'],2) }} kr @endif <span>@if(!empty($Product->discount)) &nbsp;&nbsp;<?php echo "(".$Product->discount."% off)"; ?> @endif</span></span> 
-                      <div> <span>{{ __('users.sold_by_title')}} : @if(!empty($product_seller_name)){{$product_seller_name}}@endif   </span>   <div class="clearfix"></div><span><i class="fas fa-map-marker-alt de_col"></i>
+                      <div> <span>{{ __('users.sold_by_title')}} : @if(!empty($product_seller_name)){{$product_seller_name}}@endif   </span> <span><i class="fas fa-map-marker-alt de_col"></i>
                   @if(!empty($product_location)){{$product_location}}@endif</span></div>
                     </div>
                       <div class="clearfix"></div>
@@ -140,7 +140,7 @@
             <div class="best_seller_container">
                 <h2>{{ __('lang.also_have_watch')}}</h2>
                 
-                <ul class="product_details best_seller">
+                <ul class="product_details best_seller wid_23">
 
                   @foreach($similarProducts as $product)
 

9e8954dd10b7039be58764ec0091ce682e1640aa
ashwinitechbee <ashwini.techbee@gmail.com>
2021-11-25 17:36:22 +0530

    added order listing url

--- resources/views/Front/order_success.blade.php
+++ resources/views/Front/order_success.blade.php
@@ -20,7 +20,7 @@
             
             {{ __('lang.msg_order_success')}}
             </h2>
-            <a href="{{url('/')}}/order-details/{{base64_encode($OrderId)}}" class="btn btn-black debg_color login_btn">{{ __('lang.txt_order_success_btn')}}</a>&nbsp;&nbsp;<a href="{{route('AllproductListing')}}" class="btn btn-black debg_color login_btn">{{ __('lang.buy_now_btn')}}</a>
+            <a href="{{url('/')}}/all-buyer-orders" class="btn btn-black debg_color login_btn">{{ __('lang.txt_order_success_btn')}}</a>&nbsp;&nbsp;<a href="{{route('AllproductListing')}}" class="btn btn-black debg_color login_btn">{{ __('lang.buy_now_btn')}}</a>
             @endif
           </div>
 		    </div>

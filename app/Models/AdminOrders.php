454a5918a8d90e2faa7b793b3ff3026351afeae9
ashwinitechbee <ashwini.techbee@gmail.com>
2021-11-26 17:19:16 +0530

    fixing rediretc issue

--- app/Models/AdminOrders.php
+++ app/Models/AdminOrders.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class AdminOrders extends Model
 {
     protected $table = 'admin_orders';
-    protected $fillable = ['id','user_id','address','order_lines','total','payment_details','payment_status','order_status','created_at','updated_at','klarna_order_reference','product_id'];
+    protected $fillable = ['id','user_id','address','order_lines','total','payment_details','payment_status','order_status','created_at','updated_at','klarna_order_reference','product_id','tmp_order_id'];
 
 }

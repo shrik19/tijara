9f4843a77a18beadb551fefc59a511de36c1eb64
ashwinitechbee <ashwini.techbee@gmail.com>
2021-11-26 15:15:27 +0530

    fixed wishlist issue

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -2512,7 +2512,7 @@ DATA;
                               $Service->service_link  = $service_link;
 
                               $SellerData = UserMain::select('users.id','users.fname','users.lname','users.email')->where('users.id','=',$Service->user_id)->first();
-                              $Service->seller  = $SellerData['fname'].' '.$SellerData['lname'];
+                              $Service->seller  = @$SellerData['fname'].' '.$SellerData['lname'];
                               $Service->quantity = 1;
                               $Service->images    = explode(',',$Service->images)[0];
                               $serviceDetails['service'] = $Service;

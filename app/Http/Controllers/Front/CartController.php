7f490d07e900b8b6057c73e9795d60bc6e4a0da0
ashwinitechbee <ashwini.techbee@gmail.com>
2021-12-01 16:52:40 +0530

    tring with files

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -3350,13 +3350,13 @@ DATA;
     $username ='1231181189.p12';
      $password ="swish";
     //  $url ="https://mss.cpc.getswish.net/swish-cpcapi/api/v2/paymentrequests/11A86BE70EA346E4B1C39C874173F088";
-  //   $url = "https://mss.cpc.getswish.net/swish-cpcapi/api/v1/paymentrequests";
-    $url ="https://mss.cpc.getswish.net/swish-cpcapi/api/v2/paymentrequests/".$instructionUUID;
+    $url = "https://mss.cpc.getswish.net/swish-cpcapi/api/v1/paymentrequests";
+  //  $url ="https://mss.cpc.getswish.net/swish-cpcapi/api/v2/paymentrequests/".$instructionUUID;
       
        $data =[
              "payeePaymentReference"=> "0123456789",
               "callbackUrl"=>  url("/")."/checkout-swish-number-callback",
-              "payerAlias"=> "46739866319",// 4671234768
+             // "payerAlias"=> "46739866319",// 4671234768
               "payeeAlias"=> "1233144318",// 1231181189
               "amount"=> "100",
               "currency"=> "SEK",

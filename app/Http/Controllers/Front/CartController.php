ef435c19411f94a386218bddb357f8807c199776
ashwinitechbee <ashwini.techbee@gmail.com>
2021-12-01 16:53:29 +0530

    tring with files

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -3369,7 +3369,8 @@ DATA;
          print_r($data );exit;*/
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_PUT, true);
+       // curl_setopt($ch, CURLOPT_PUT, true);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));

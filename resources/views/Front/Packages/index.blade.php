8afca0be627f6cdd5326e5d474d1ad60f40a078f
ashwinitechbee <ashwini.techbee@gmail.com>
2021-12-03 15:39:19 +0530

    added css col

--- resources/views/Front/Packages/index.blade.php
+++ resources/views/Front/Packages/index.blade.php
@@ -89,7 +89,7 @@
 	      	 <div class="col-md-6 ">
 				   <br/><br/>
 				   <?php 
-				   $class='';
+				   $class='test';
 				   	if($row->start_date >= date('Y-m-d H:i:s') && $row->payment_status=='CAPTURED' ){
 				   		$class = $inactive;
 				   	} elseif($row->start_date <= date('Y-m-d H:i:s') && $row->payment_status=='CAPTURED' && $row->end_date >= date('Y-m-d H:i:s')){

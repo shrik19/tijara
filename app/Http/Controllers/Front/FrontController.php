5f074c6d3526466fc36fbac40f3a8522824e4233
ashwinitechbee <ashwini.techbee@gmail.com>
2021-12-09 14:44:28 +0530

    added amount

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -2820,7 +2820,7 @@ public function getCatSubList(Request $request) {
 						Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
 		
 						$response = Stripe\Charge::create ([
-							"amount" => 1*100,//($subscription->amount*100),
+							"amount" => 6*100,//($subscription->amount*100),
 							"currency" => "SEK",
 							"customer" => $subscription->stripe_customer_id,
 							"description" => "Package Subscription payment for UserId 

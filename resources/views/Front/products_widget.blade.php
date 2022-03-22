6d5ca326e29cf9465a8c43c6c9fd433da0b34f95
ashwinitechbee <ashwini.techbee@gmail.com>
2022-03-22 11:13:40 +0530

    fixed alignment issue

--- resources/views/Front/products_widget.blade.php
+++ resources/views/Front/products_widget.blade.php
@@ -16,16 +16,8 @@ $order_product_link = url('/').'/product/'.$product->product_slug.'-P-'.$product
   <div class="product_data product_link_js" product_link="@if(!empty($product_link)){{$product_link}}@else{{$order_product_link}}@endif" @if($product->is_sold == '1') style="pointer-events: none;opacity: 0.4;"  @endif>
     <div class="product_img" style="display:inline-block;background-color: white;">
       <?php
-     $searchForValue = ',';
-$stringValue = '115,251';
-
-if( strpos($stringValue, $searchForValue) !== false ) {
-     echo "Found";
-}
-
-exit;
-echo $product->image;exit;
-      if( strpos(',', $product->image) !== false ) {
+  
+      if( strpos( $searchForValue , $product->image) !== false ) {
         echo "in";exit;
            $featureProductImage = explode(',', $product->image)[0];
       }else{

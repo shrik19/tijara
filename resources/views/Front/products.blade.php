24e7233454db40a5ef318bfc54c15c4bb306a6d7
Kevin <kevin@telemar.it>
2022-03-23 10:33:29 +0530

    css

--- resources/views/Front/products.blade.php
+++ resources/views/Front/products.blade.php
@@ -63,7 +63,7 @@
 						<div class="col-md-4"></div>
 					@endif
 						  <div class="col-md-2"></div>
-						  <div style="margin-top: -3%;" class="@if(Request::segment(1) !='annonser') col-md-offset-1 @endif col-md-3 prod-service-filter >
+						  <div style="margin-top: -3%;" class="@if(Request::segment(1) !='annonser') col-md-offset-1 @endif col-md-3 prod-service-filter" >
 						  
 							<div class="form-group">
 							  <label class="product_sorting_filter">{{ __('lang.sort_by_order')}} : </label>

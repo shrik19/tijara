4f6276107963210a225c781f961afd1a80674925
Priyanka <spriyanka.shinde25@gmail.com>
2022-09-09 11:58:27 +0530

    fixes

--- resources/views/Front/seller-products.blade.php
+++ resources/views/Front/seller-products.blade.php
@@ -336,8 +336,19 @@
   
 <script type="text/javascript">
 function deleteCookie(name) {
+	console.log('getCookie='+getCookie(name));
     document.cookie = name+'=null;-1; path=/';
 	console.log('deleted='+name);
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
 setTimeout(function() { 
 deleteCookie("seller_banner_preview");

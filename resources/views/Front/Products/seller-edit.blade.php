0310c5135a65bed6ef7be7f577d27ecb28186986
Azhar <azzzyrocks@gmail.com>
2023-01-22 22:22:14 +0530

    Fixes 2201-2

--- resources/views/Front/Products/seller-edit.blade.php
+++ resources/views/Front/Products/seller-edit.blade.php
@@ -243,7 +243,7 @@
                                  <?php }?>
                                 
                                     <div class="col-md-8 tj-svselect tj-newslect">
-                                      <div class="row tj-wid67">
+                                      <div class="row tj-wid67 tjnm-full">
                                         <div class="col-sm-6">
                                          <!---- <select id="{{$attribute['id']}}" style="  width: 34%;"  class="col-md-4 ge_input select_attribute tjselect" name="attribute[<?php echo $i;?>][]" variant_id="<?php echo $i;?>"> --->
 										 <select id="<?php echo $j;?>" style="  width: 34%;"  class="col-md-4 ge_input select_attribute tjselect" name="attribute[<?php echo $i;?>][]" variant_id="<?php echo $i;?>">

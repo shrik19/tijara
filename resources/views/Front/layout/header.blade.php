a1dc9e72ee35bf7f3a24e2eba0b968f774e4afde
kevin <kevin@telemar.it>
2023-03-16 18:53:54 +0530

    added favicon

--- resources/views/Front/layout/header.blade.php
+++ resources/views/Front/layout/header.blade.php
@@ -11,6 +11,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="_token" content="{{ csrf_token() }}">
   
+  <link rel="apple-touch-icon" href="{{url('/')}}/assets/icon/apple-touch-icon.png">
   <link rel="apple-touch-icon" sizes="180x180" href="{{url('/')}}/assets/icon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="{{url('/')}}/assets/icon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="{{url('/')}}/assets/icon/favicon-16x16.png">
